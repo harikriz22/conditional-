a399d75537753e8e0ac7ed1d1b39de6cd1be442e
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2026-06-16 11:41:48 +0530

    Add files via upload

---
+++ three.py
@@ -0,0 +1,64 @@
+# # data types 
+
+# # user 
+
+# # int 
+# # float 
+# # bool
+# # str
+# # list 
+# # tuple
+# # dict 
+# # set 
+
+# # numeric 
+
+# # int 
+# # float 
+
+# # a = 6 
+# # name = 'mohan'
+# # print(type(name))
+
+
+# # a = 0 
+# # print(a)
+# # print(type(a))
+
+# # floating 
+
+# # b = 5.0
+# # print(b)
+# # print(type(b))
+
+# # bool / boolean
+
+# # a = True
+# # b = False
+
+# b = 0
+# print(bool(b))
+
+
+# # arithemtic operators
+# a = 9
+# b = 4
+# # Addition  +
+# print(a + b)
+# # subtraction -
+# print(a - b)
+# # multiplication *
+# print(a * b)
+# # division /
+# print(a / b)
+# # exponent **
+# print(2**3)
+# # floor division //
+# print(9//4)
+# # modulus %
+# print(10%7)
+
+
+
+
+
